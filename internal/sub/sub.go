3b7f6b8fee83fc9457c2a38b4a52b111920056b3
thefather12 <94747184+thefather12@users.noreply.github.com>
2025-11-18 23:26:26 -0600

    v1

---
+++ internal/sub/sub.go
@@ -0,0 +1,314 @@
+package sub
+
+import (
+	"context"
+	"crypto/tls"
+	"embed"
+	"html/template"
+	"io"
+	"io/fs"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"x-ui/config"
+	"x-ui/internal/logger"
+	"x-ui/internal/util/common"
+	"x-ui/internal/web/middleware"
+	"x-ui/internal/web/network"
+	"x-ui/internal/web/service"
+
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed html/*
+var htmlFS embed.FS
+
+type Server struct {
+	httpServer *http.Server
+	listener   net.Listener
+
+	sub            *SUBController
+	settingService service.SettingService
+
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func NewServer() *Server {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &Server{
+		ctx:    ctx,
+		cancel: cancel,
+	}
+}
+
+func (s *Server) getHtmlFiles(customFolder string) ([]string, error) {
+	files := make([]string, 0)
+	dir, _ := os.Getwd()
+	err := fs.WalkDir(os.DirFS(dir), customFolder, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
+}
+
+func (s *Server) getHtmlTemplate(funcMap template.FuncMap, customFolder string) (*template.Template, error) {
+	t := template.New("").Funcs(funcMap)
+	err := fs.WalkDir(os.DirFS(customFolder), ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			newT, err := t.ParseFS(os.DirFS(customFolder), path+"/*.html")
+			if err != nil {
+				// ignore
+				return nil
+			}
+			t = newT
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
+func (s *Server) initRouter() (*gin.Engine, error) {
+	if config.IsDebug() {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.DefaultWriter = io.Discard
+		gin.DefaultErrorWriter = io.Discard
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	engine := gin.Default()
+
+	basePath, err := s.settingService.GetBasePath()
+	if err != nil {
+		return nil, err
+	}
+	engine.Use(gzip.Gzip(gzip.DefaultCompression, gzip.WithExcludedPaths([]string{basePath + "panel/API/"})))
+
+	engine.Use(func(c *gin.Context) {
+		c.Set("base_path", basePath)
+	})
+
+	// set static files and template
+	CustomUI, err := s.settingService.GetSubCustomUI()
+	if err != nil {
+		return nil, err
+	}
+
+	customFolder := "sub/html"
+	if CustomUI {
+		customFolder = "/etc/x-ui/html"
+		// Create the custom folder if it does not exist
+		if _, err := os.Stat(customFolder); os.IsNotExist(err) {
+			err := os.MkdirAll(customFolder, os.ModePerm)
+			if err != nil {
+				logger.Error("Failed to create custom folder:", err)
+				return nil, err
+			}
+		}
+	}
+
+	if config.IsDebug() {
+		files, err := s.getHtmlFiles(customFolder)
+		if err != nil {
+			logger.Error("Failed to get HTML files:", err)
+			return nil, err
+		}
+		engine.LoadHTMLFiles(files...)
+	} else {
+		// Check if custom folder exists and is not empty
+		if _, err := os.Stat(customFolder); err == nil {
+			template, err := s.getHtmlTemplate(engine.FuncMap, customFolder)
+			if err != nil {
+				logger.Error("Failed to get HTML template from custom folder:", err)
+				return nil, err
+			}
+			engine.SetHTMLTemplate(template)
+		} else {
+			// Fallback to embedded HTML files
+			tmpl := template.Must(template.New("").ParseFS(htmlFS, "html/*.html"))
+			engine.SetHTMLTemplate(tmpl)
+		}
+	}
+
+	subDomain, err := s.settingService.GetSubDomain()
+	if err != nil {
+		return nil, err
+	}
+
+	if subDomain != "" {
+		engine.Use(middleware.DomainValidatorMiddleware(subDomain))
+	}
+
+	LinksPath, err := s.settingService.GetSubPath()
+	if err != nil {
+		return nil, err
+	}
+
+	JsonPath, err := s.settingService.GetSubJsonPath()
+	if err != nil {
+		return nil, err
+	}
+
+	Encrypt, err := s.settingService.GetSubEncrypt()
+	if err != nil {
+		return nil, err
+	}
+
+	ShowInfo, err := s.settingService.GetSubShowInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	RemarkModel, err := s.settingService.GetRemarkModel()
+	if err != nil {
+		RemarkModel = "-ieo"
+	}
+
+	SubUpdates, err := s.settingService.GetSubUpdates()
+	if err != nil {
+		SubUpdates = "10"
+	}
+
+	SubJsonFragment, err := s.settingService.GetSubJsonFragment()
+	if err != nil {
+		SubJsonFragment = ""
+	}
+
+	SubJsonNoises, err := s.settingService.GetSubJsonNoises()
+	if err != nil {
+		SubJsonNoises = ""
+	}
+
+	SubJsonMux, err := s.settingService.GetSubJsonMux()
+	if err != nil {
+		SubJsonMux = ""
+	}
+
+	SubJsonRules, err := s.settingService.GetSubJsonRules()
+	if err != nil {
+		SubJsonRules = ""
+	}
+
+	SubTitle, err := s.settingService.GetSubTitle()
+	if err != nil {
+		SubTitle = ""
+	}
+
+	g := engine.Group("/")
+
+	s.sub = NewSUBController(
+		g, LinksPath, JsonPath, Encrypt, ShowInfo, RemarkModel, SubUpdates,
+		SubJsonFragment, SubJsonNoises, SubJsonMux, SubJsonRules, SubTitle)
+
+	return engine, nil
+}
+
+func (s *Server) Start() (err error) {
+	// This is an anonymous function, no function name
+	defer func() {
+		if err != nil {
+			s.Stop()
+		}
+	}()
+
+	subEnable, err := s.settingService.GetSubEnable()
+	if err != nil {
+		return err
+	}
+	if !subEnable {
+		return nil
+	}
+
+	engine, err := s.initRouter()
+	if err != nil {
+		return err
+	}
+
+	certFile, err := s.settingService.GetSubCertFile()
+	if err != nil {
+		return err
+	}
+	keyFile, err := s.settingService.GetSubKeyFile()
+	if err != nil {
+		return err
+	}
+	listen, err := s.settingService.GetSubListen()
+	if err != nil {
+		return err
+	}
+	port, err := s.settingService.GetSubPort()
+	if err != nil {
+		return err
+	}
+
+	listenAddr := net.JoinHostPort(listen, strconv.Itoa(port))
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return err
+	}
+
+	if certFile != "" || keyFile != "" {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err == nil {
+			c := &tls.Config{
+				Certificates: []tls.Certificate{cert},
+			}
+			listener = network.NewAutoHttpsListener(listener)
+			listener = tls.NewListener(listener, c)
+			logger.Info("Sub server running HTTPS on", listener.Addr())
+		} else {
+			logger.Error("Error loading certificates:", err)
+			logger.Info("Sub server running HTTP on", listener.Addr())
+		}
+	} else {
+		logger.Info("Sub server running HTTP on", listener.Addr())
+	}
+	s.listener = listener
+
+	s.httpServer = &http.Server{
+		Handler: engine,
+	}
+
+	go func() {
+		s.httpServer.Serve(listener)
+	}()
+
+	return nil
+}
+
+func (s *Server) Stop() error {
+	s.cancel()
+
+	var err1 error
+	var err2 error
+	if s.httpServer != nil {
+		err1 = s.httpServer.Shutdown(s.ctx)
+	}
+	if s.listener != nil {
+		err2 = s.listener.Close()
+	}
+	return common.Combine(err1, err2)
+}
+
+func (s *Server) GetCtx() context.Context {
+	return s.ctx
+}
---
+++ internal/sub/subController.go
@@ -0,0 +1,259 @@
+package sub
+
+import (
+	"encoding/base64"
+	"fmt"
+	"math"
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SUBController struct {
+	subTitle       string
+	subPath        string
+	subJsonPath    string
+	subEncrypt     bool
+	updateInterval string
+
+	subService     *SubService
+	subJsonService *SubJsonService
+}
+
+func NewSUBController(
+	g *gin.RouterGroup,
+	subPath string,
+	jsonPath string,
+	encrypt bool,
+	showInfo bool,
+	rModel string,
+	update string,
+	jsonFragment string,
+	jsonNoise string,
+	jsonMux string,
+	jsonRules string,
+	subTitle string,
+) *SUBController {
+	sub := NewSubService(showInfo, rModel)
+	a := &SUBController{
+		subTitle:       subTitle,
+		subPath:        subPath,
+		subJsonPath:    jsonPath,
+		subEncrypt:     encrypt,
+		updateInterval: update,
+
+		subService:     sub,
+		subJsonService: NewSubJsonService(jsonFragment, jsonNoise, jsonMux, jsonRules, sub),
+	}
+	a.initRouter(g)
+	return a
+}
+
+func (a *SUBController) initRouter(g *gin.RouterGroup) {
+	gLink := g.Group(a.subPath)
+	gJson := g.Group(a.subJsonPath)
+
+	gLink.GET(":subid", a.subs)
+	gLink.GET(":subid/json", a.subsJson) // New JSON endpoint
+
+	gJson.GET(":subid", a.subJsons)
+}
+
+func (a *SUBController) subs(c *gin.Context) {
+	subId := c.Param("subid")
+	var host string
+	if h, err := getHostFromXFH(c.GetHeader("X-Forwarded-Host")); err == nil {
+		host = h
+	}
+	if host == "" {
+		host = c.GetHeader("X-Real-IP")
+	}
+	if host == "" {
+		var err error
+		host, _, err = net.SplitHostPort(c.Request.Host)
+		if err != nil {
+			host = c.Request.Host
+		}
+	}
+	subs, header, emails, err := a.subService.GetSubs(subId, host)
+	if err != nil || len(subs) == 0 {
+		c.String(400, "Error!")
+	} else {
+		result := ""
+		for _, sub := range subs {
+			result += sub + "\n"
+		}
+		resultSlice := strings.Split(strings.TrimSpace(result), "\n")
+
+		clientEmails := ""
+		for _, email := range emails {
+			clientEmails += email + "\n"
+		}
+		ResultsEmails := strings.Split(strings.TrimSpace(clientEmails), "\n")
+
+		// Add headers
+		c.Writer.Header().Set("Subscription-Userinfo", header)
+		c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
+		c.Writer.Header().Set("Profile-Title", "base64:"+base64.StdEncoding.EncodeToString([]byte(a.subTitle)))
+
+		acceptHeader := c.GetHeader("Accept")
+		headerMap := parseHeaderString(header)
+		expireValue := headerMap["expire"]
+		upValue := formatBytes(headerMap["upload"], 2)
+		downValue := formatBytes(headerMap["download"], 2)
+		totalValue := formatBytes(headerMap["total"], 2)
+		subJsonUrl := "https://" + c.Request.Host + a.subJsonPath + subId
+		currentURL := "https://" + c.Request.Host + c.Request.RequestURI
+
+		if strings.Contains(acceptHeader, "text/html") {
+			if a.subEncrypt {
+				c.String(200, base64.StdEncoding.EncodeToString([]byte(result)))
+			} else {
+				c.HTML(200, "sub.html", gin.H{
+					"result":       resultSlice,
+					"total":        totalValue,
+					"expire":       expireValue,
+					"upload":       upValue,
+					"download":     downValue,
+					"totalByte":    headerMap["total"],
+					"uploadByte":   headerMap["upload"],
+					"downloadByte": headerMap["download"],
+					"sId":          subId,
+					"emails":       ResultsEmails,
+					"subUrl":       currentURL,
+					"jsonUrl":      subJsonUrl,
+				})
+			}
+		} else {
+			if a.subEncrypt {
+				c.String(200, base64.StdEncoding.EncodeToString([]byte(result)))
+			} else {
+				c.String(200, result)
+			}
+		}
+	}
+}
+
+func (a *SUBController) subJsons(c *gin.Context) {
+	subId := c.Param("subid")
+	var host string
+	if h, err := getHostFromXFH(c.GetHeader("X-Forwarded-Host")); err == nil {
+		host = h
+	}
+	if host == "" {
+		host = c.GetHeader("X-Real-IP")
+	}
+	if host == "" {
+		var err error
+		host, _, err = net.SplitHostPort(c.Request.Host)
+		if err != nil {
+			host = c.Request.Host
+		}
+	}
+	jsonSub, header, err := a.subJsonService.GetJson(subId, host)
+	if err != nil || len(jsonSub) == 0 {
+		c.String(400, "Error!")
+	} else {
+
+		// Add headers
+		c.Writer.Header().Set("Subscription-Userinfo", header)
+		c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
+		c.Writer.Header().Set("Profile-Title", "base64:"+base64.StdEncoding.EncodeToString([]byte(a.subTitle)))
+
+		c.String(200, jsonSub)
+	}
+}
+
+// New handler for JSON response
+func (a *SUBController) subsJson(c *gin.Context) {
+	subId := c.Param("subid")
+	var host string
+	if h, err := getHostFromXFH(c.GetHeader("X-Forwarded-Host")); err == nil {
+		host = h
+	}
+	if host == "" {
+		host = c.GetHeader("X-Real-IP")
+	}
+	if host == "" {
+		var err error
+		host, _, err = net.SplitHostPort(c.Request.Host)
+		if err != nil {
+			host = c.Request.Host
+		}
+	}
+	subs, header, emails, err := a.subService.GetSubs(subId, host)
+	if err != nil || len(subs) == 0 {
+		c.JSON(400, gin.H{"error": "Error!"})
+		return
+	}
+
+	resultSlice := strings.Split(strings.TrimSpace(strings.Join(subs, "\n")), "\n")
+
+	headerMap := parseHeaderString(header)
+	expireValue := headerMap["expire"]
+	upValue := formatBytes(headerMap["upload"], 2)
+	downValue := formatBytes(headerMap["download"], 2)
+	totalValue := formatBytes(headerMap["total"], 2)
+	subJsonUrl := "https://" + c.Request.Host + a.subJsonPath + subId
+	currentURL := "https://" + c.Request.Host + a.subPath + subId
+
+	c.Writer.Header().Set("Subscription-Userinfo", header)
+	c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
+	c.Writer.Header().Set("Profile-Title", "base64:"+base64.StdEncoding.EncodeToString([]byte(a.subTitle)))
+
+	c.JSON(200, gin.H{
+		"result":       resultSlice,
+		"total":        totalValue,
+		"expire":       expireValue,
+		"upload":       upValue,
+		"download":     downValue,
+		"totalByte":    headerMap["total"],
+		"uploadByte":   headerMap["upload"],
+		"downloadByte": headerMap["download"],
+		"sId":          subId,
+		"emails":       emails,
+		"subUrl":       currentURL,
+		"jsonUrl":      subJsonUrl,
+	})
+}
+
+func getHostFromXFH(s string) (string, error) {
+	if strings.Contains(s, ":") {
+		realHost, _, err := net.SplitHostPort(s)
+		if err != nil {
+			return "", err
+		}
+		return realHost, nil
+	}
+	return s, nil
+}
+
+func parseHeaderString(header string) map[string]string {
+	headerMap := make(map[string]string)
+	pairs := strings.Split(header, ";")
+	for _, pair := range pairs {
+		kv := strings.Split(strings.TrimSpace(pair), "=")
+		if len(kv) == 2 {
+			headerMap[kv[0]] = kv[1]
+		}
+	}
+	return headerMap
+}
+
+func formatBytes(sizeStr string, precision int) string {
+	// Convert the string input to a float64
+	size, _ := strconv.ParseFloat(sizeStr, 64)
+
+	if size == 0 {
+		return "0 B"
+	}
+
+	// Calculate base and suffix
+	base := math.Log(size) / math.Log(1024)
+	suffixes := []string{"B", "K", "M", "G", "T"}
+
+	value := math.Pow(1024, base-math.Floor(base))
+	return fmt.Sprintf("%.*f %s", precision, value, suffixes[int(math.Floor(base))])
+}
